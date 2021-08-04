b25fac94b1d7511817345069da7641ba2fea76d8
unknown <s-sangeeth-k@sicis.co.jp>
2021-08-04 17:08:45 +0900

    changed Base.html to base.html

--- menu/views.py
+++ menu/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def menu_home(request):
-    return render(request,"menu/menu_home.html")
+    context= {
+        'test' : 'sangeeth'
+    }
+    return render(request,"menu/menu_home.html" , context)
